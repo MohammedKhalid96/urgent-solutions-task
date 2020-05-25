2c7f3469d160d21c41937d0b6d40f6444e880c09
MohammedKhalid96 <mohammedkhalid96@outlook.com>
2020-05-25 21:31:40 +0200

    Some changes

---
+++ src/App.vue
@@ -0,0 +1,290 @@
+<template>
+  <div id="app">
+    <div class="slider">
+      <hooper
+        :vertical="true"
+        style="height: 100%"
+        :itemsToShow="1"
+        :centerMode="true"
+      >
+        <slide>
+          <div class="first-slide">
+            <div class="back-layer">
+              <div class="back-text">
+                <h3 class="big-transparent-text">
+                  Mohamed
+                </h3>
+                <h3 class="big-transparent-text">
+                  Khaled
+                </h3>
+              </div>
+
+              <div class="overlay-layer">
+                <div class="front-right-text">
+                  <h3>
+                    Get
+                    <span class="dot"></span>
+                  </h3>
+                  <h3>
+                    The 
+                  </h3>
+                  <h3>
+                    Weather
+                  </h3>
+                </div>
+
+                <div class="front-left-text">
+                  <p>
+                    Lorem ipsum dolor sit, amet<br />
+                    elitprehenderit quo e <br />aliquam deserunt quod.
+                  </p>
+                </div>
+              </div>
+
+              <div class="circles-layers">
+                <div class="circle circle-1"></div>
+                <div class="circle circle-2"></div>
+                <div class="circle circle-3"></div>
+                <div class="circle circle-4"></div>
+              </div>
+            </div>
+          </div>
+        </slide>
+
+        <slide>
+          <GetWeather />
+        </slide>
+
+        <hooper-pagination slot="hooper-addons"></hooper-pagination>
+      </hooper>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Hooper, Slide, Pagination as HooperPagination } from "hooper";
+import "hooper/dist/hooper.css";
+
+import GetWeather from "./components/GetWeather";
+
+export default {
+  name: "App",
+  components: {
+    Hooper,
+    Slide,
+    HooperPagination,
+    GetWeather
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+
+.slider {
+  height: 100vh;
+}
+
+.slider {
+  outline: 0;
+}
+
+.hooper {
+  outline: 0;
+}
+
+.hooper-pagination.is-vertical .hooper-indicator {
+  width: 35px;
+  height: 7px;
+}
+
+.hooper-pagination.is-vertical .hooper-indicator.is-active {
+  width: 50px;
+}
+
+.hooper-indicator:hover, .hooper-indicator {
+  background: #0527e6;
+  outline: none;
+  border: 0;
+}
+
+.hooper-indicator:hover, .hooper-indicator.is-active {
+  background: #ff4500;
+}
+
+.back-layer {
+  position: relative;
+}
+
+.big-transparent-text {
+  color: #1b9e98;
+  font-size: 19em;
+  padding: 1rem 0 0 5rem;
+  opacity: 0.1;
+}
+
+.overlay-layer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.front-right-text,
+.front-left-text {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.front-right-text {
+  right: 20%;
+}
+
+.front-right-text h3 {
+  font-size: 8em;
+  text-transform: uppercase;
+}
+
+.front-left-text {
+  left: 13%;
+  transform: rotate(270deg);
+}
+
+.dot {
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  background: #ff4500;
+  border-radius: 50%;
+}
+
+.circle {
+  position: absolute;
+  border: 0.4em solid transparent;
+  border-radius: 0.5em;
+  background: linear-gradient(to right, #111111, #111111),
+    linear-gradient(15deg, #0527e6, #1b9e98);
+  background-clip: padding-box, border-box;
+  background-origin: padding-box, border-box;
+}
+
+.circle-1 {
+  width: 3em;
+  height: 3em;
+  bottom: 32.7%;
+  left: 13%;
+  opacity: 0.9;
+}
+
+.circle-2 {
+  width: 2.2em;
+  height: 2.2em;
+  top: 20%;
+  left: 30%;
+  border-radius: 0.7em;
+  opacity: 0.6;
+}
+
+.circle-3 {
+  width: 2.5em;
+  height: 2.5em;
+  bottom: 20%;
+  left: 35%;
+}
+
+.circle-4 {
+  width: 2.5em;
+  height: 2.5em;
+  top: 14%;
+  right: 30%;
+  opacity: 0.8;
+}
+
+@media (min-width: 992px) and (max-width: 1183px) { 
+  .front-left-text {
+    left: 0;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 991px) {
+  .slider {
+    padding: 1em;
+  }
+
+  .overlay-layer {
+    position: static;
+  }
+
+  .big-transparent-text {
+    font-size: 20em;
+    opacity: 0.3;
+    padding: 1rem 0 0 5rem;
+  }
+
+  .front-right-text,
+  .front-left-text {
+    transform: translateY(0);
+    left: 0;
+  }
+
+  .front-left-text {
+    position: static;
+  }
+
+  .front-left-text {
+    transform: rotate(0);
+  }
+
+  .front-right-text h3 {
+    font-size: 5em;
+  }
+
+  .circle {
+    opacity: 0.4;
+  }
+}
+
+@media (min-width: 320px) and (max-width: 767px) {
+  .slider {
+    padding: 1em;
+  }
+
+  .overlay-layer {
+    position: static;
+  }
+
+  .big-transparent-text {
+    font-size: 10em;
+    opacity: 0.3;
+    padding: 1rem 0 0 5rem;
+  }
+
+  .front-right-text,
+  .front-left-text {
+    transform: translateY(0);
+    left: 0;
+  }
+
+  .front-left-text {
+    position: static;
+  }
+
+  .front-left-text {
+    transform: rotate(0);
+  }
+
+  .front-right-text h3 {
+    font-size: 3em;
+  }
+
+  .circle {
+    opacity: 0.1;
+  }
+}
+</style>
---
+++ src/components/GetWeather.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="get-weather-slide">
+    <div class="get-weather-wrap">
+      <div class="title">
+        <h3>{{title.toLocaleUpperCase()}}</h3>
+        <p>{{note}}</p>
+      </div>
+
+      <div class="location-form">
+        <input
+          type="text"
+          placeholder="Enter the city name (You must spelling correctly)"
+          v-model="query"
+          @keyup.enter="fetchEntredLocationWeather"
+        />
+        <br />
+        <button :disabled="query.length === 0" @click="fetchEntredLocationWeather">Get {{query}} Weather Now</button>
+      </div>
+
+      <div v-if="typeof weather.main != 'undefined'" class="weather-info">
+        <div>{{ weather.name }}, {{ weather.sys.country }}</div>
+        <div>{{ weather.weather[0].main }}</div>
+
+        <div>{{ Math.round(weather.main.temp) }}</div>
+        <div>
+          {{ Math.round(weather.main.temp_max) }},
+          {{ Math.round(weather.main.temp_min) }}
+        </div>
+        <div>{{ weather.weather[0].description }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GetWeather",
+
+  data() {
+    return {
+      api_Key: "4b27475bb80b9d888231ad387cf93fc9",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+      coordinates: {
+        lat: 0,
+        lng: 0
+      },
+      title: "Just type the city name",
+      note:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    };
+  },
+
+  methods: {
+    fetchEntredLocationWeather() {
+      fetch(
+        `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_Key}`
+      )
+        .then(res => {
+          return res.json();
+        })
+        .then(this.setResults);
+    },
+    setResults(results) {
+      this.weather = results;
+    }
+  },
+  mounted() {
+    let long;
+    let lat;
+    for (var i = 0; i < 2; i++) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          // Fetch weather
+          this.locationFound = true;
+          long = position.coords.longitude;
+          lat = position.coords.latitude;
+          fetch(
+            `${this.url_base}weather?lat=${lat}&lon=${long}&units=metric&APPID=${this.api_Key}`
+          )
+            .then(res => {
+              return res.json();
+            })
+            .then(res => {
+              this.setResults(res);
+            });
+        }),
+          error => {
+            if (error.code == error.PERMISSION_DENIED) {
+              this.locationFound = false;
+            }
+          };
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.get-weather-slide {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  padding: 0 5em;
+  text-align: center;
+}
+
+.location-form {
+  margin: 3em 0;
+}
+
+input {
+  width: 50%;
+  padding: 1em;
+  margin-bottom: 1em;
+  outline: 0;
+  border: 0.2em solid transparent;
+  border-radius: 2em;
+  background: linear-gradient(to right, #111111, #111111),
+    linear-gradient(15deg, #0527e6, #1b9e98);
+  background-clip: padding-box, border-box;
+  background-origin: padding-box, border-box;
+  color: #fff;
+}
+
+button {
+  background: #ff4500;
+  outline: 0;
+  border: 0;
+  padding: 1em 2em;
+  color: #fff;
+  border-radius: 2em;
+  cursor: pointer;
+}
+
+button:disabled {
+  background: #ff4500;
+  opacity: 0.3;
+}
+
+.title h3 {
+  font-size: 2em;
+}
+
+.title p {
+  color: #1b9e98;
+  margin: 1em 0;
+}
+
+.weather-info div {
+  margin: 1em 0;
+}
+</style>
